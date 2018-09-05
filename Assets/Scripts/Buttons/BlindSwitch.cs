28da05598f3ee42445a7b634fdf47db927598de9
Jano van der Dussen <duss0001@hz.nl>
2018-09-05 13:05:27 +0200

    added classroom demo

---
+++ Assets/Scripts/Buttons/BlindSwitch.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BlindSwitch : MonoBehaviour
+{
+    public GameObject blind;
+    public GameObject textO;
+    public GameObject textC;
+    public int speed;
+
+    void Start()
+    {
+        textC.SetActive(false);
+        textO.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            textC.SetActive(true);
+            textO.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            textO.SetActive(false);
+            textC.SetActive(false);
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {         
+               blind.transform.Translate(Vector3.up * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+               blind.transform.Translate(Vector3.down * Time.deltaTime);
+            }
+        }
+    }
+}
